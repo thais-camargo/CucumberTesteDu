75984fd69d9f43afc109905581025fbab8d9ca88
T73000035 <eduardo.libanori@yaman.com.br>
2019-05-24 11:57:06 -0300

    Primeiro Cenario

---
+++ CucumberTests/features/specs/primeiro_cenario.feature
@@ -0,0 +1,10 @@
+# language:pt
+
+Funcionalidade: Fazer uma soma.
+
+#-Eu como usuario
+#-Eu quero fazer uma soma
+
+Cenario: Fazer uma soma
+Quando eu somo 2 mais 2
+Então o resultado deve ser 4
---
+++ CucumberTests/features/step_definitions/primeirocenario.rb
@@ -0,0 +1,9 @@
+Quando("eu somo {int} mais {int}") do |valor1, valor2|
+  @soma =valor1 + valor2
+end
+
+Então("o resultado deve ser {int}") do |resultado|
+  expect(@soma).to eq resultado
+  puts @soma
+  puts resultado
+end
